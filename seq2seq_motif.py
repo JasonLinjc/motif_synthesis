6f0d93a85e0b2c6da5b168156d952f90d1257da8
JasonLinjc <jieconlin3-c@my.cityu.edu.hk>
2019-02-22 20:40:01 +0800

    update seq2seq

--- seq2seq_motif.py
+++ seq2seq_motif.py
@@ -3,10 +3,14 @@
 # @Auther   :Jason Lin
 # @File     :seq2seq_motif.py
 # @Software :PyCharm
-
+from __future__ import print_function
 import pickle as pkl
 import numpy as np
 import pandas as pd
+from keras.models import Sequential
+from keras import layers
+from six.moves import range
+
 
 # dimer motif max_length: 31
 # two motif max length  : 32
@@ -104,7 +108,38 @@ def seq2seq_model():
     x_train, y_train = get_motif_from_family()
     print(x_train.shape)
     print(y_train.shape)
-    
+    RNN = layers.LSTM
+    HIDDEN_SIZE = 128
+    BATCH_SIZE = 128
+    LAYERS = 1
+    MAXLEN = 33
+    Nucleic = 4
+
+    print('Build model...')
+    model = Sequential()
+    # "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE.
+    # Note: In a situation where your input sequences have a variable length,
+    # use input_shape=(None, num_feature).
+    model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, Nucleic)))
+    # As the decoder RNN's input, repeatedly provide with the last output of
+    # RNN for each time step. Repeat 'DIGITS + 1' times as that's the maximum
+    # length of output, e.g., when DIGITS=3, max output is 999+999=1998.
+    model.add(layers.RepeatVector(DIGITS + 1))
+    # The decoder RNN could be multiple layers stacked or a single layer.
+    for _ in range(LAYERS):
+        # By setting return_sequences to True, return not only the last output but
+        # all the outputs so far in the form of (num_samples, timesteps,
+        # output_dim). This is necessary as TimeDistributed in the below expects
+        # the first dimension to be the timesteps.
+        model.add(RNN(HIDDEN_SIZE, return_sequences=True))
+
+    # Apply a dense layer to the every temporal slice of an input. For each of step
+    # of the output sequence, decide which character should be chosen.
+    model.add(layers.TimeDistributed(layers.Dense(len(chars), activation='softmax')))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    model.summary()
 
 
     pass
