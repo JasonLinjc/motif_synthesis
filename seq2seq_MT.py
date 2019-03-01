b58a31eb2888e2e87798feee54095cf3ed4548ad
JasonLinjc <jieconlin3-c@my.cityu.edu.hk>
2019-03-01 14:55:40 +0800

    update model

--- seq2seq_MT.py
+++ seq2seq_MT.py
@@ -11,8 +11,89 @@ import os, sys, time, random
 import h5py
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
+from keras.optimizers import Adam
 
 
-batch_size = 1
-epochs = 200
-latent_dim = 128
+def generate_input_motif_seq():
+    max_dimer_len = 31
+    max_motif_pair_len = 32
+    data = []
+    motif_data = pkl.load(open("./dimer_motif_pair.pkl", "rb"))
+    for motif in motif_data:
+        dimer_dict = motif[0]
+        motif1_dict = motif[1]
+        motif2_dict = motif[2]
+        dimer_family = motif[3]
+        # print(dimer_dict)
+        dimer_name = list(dimer_dict.keys())[0]
+        motif1_name = list(motif1_dict.keys())[0]
+        motif2_name = list(motif2_dict.keys())[0]
+        motif_pair_name = motif1_name + ":" + motif2_name
+
+        dimer_seq = dimer_dict[dimer_name]
+        motif1_seq = motif1_dict[motif1_name]
+        motif2_seq = motif2_dict[motif2_name]
+
+        # print(dimer_seq)
+        divied_vec = np.ones(4)
+        if motif1_seq == " " or motif2_seq == " ":
+            continue
+        motif_pair_seq = np.concatenate((motif1_seq, [divied_vec], motif2_seq))
+        # print(len(motif_pair_seq))
+        # motif_padding_len = max_motif_pair_len + 1 - len(motif_pair_seq)
+        # padding_vec = np.zeros((motif_padding_len, 4))
+        # motif_pair_seq = np.concatenate((motif_pair_seq, padding_vec))
+        # print(motif_pair_seq.shape)
+        # print(motif_pair_seq)
+        start_code = np.array([0.,0.,0.,0.,1.,0.])
+        end_code = np.array([0.,0.,0.,0.,0.,1.])
+        dimer_input = np.concatenate((dimer_seq, np.zeros((len(dimer_seq), 2))), axis=1)
+        dimer_target = np.concatenate((dimer_input, end_code), axis=0)
+        dimer_input = np.concatenate((start_code, dimer_input, end_code), axis=0)
+
+        # dimer_padding_len = max_dimer_len - len(dimer_seq)
+        # padding_vec = np.zeros((dimer_padding_len, 4))
+        # dimer_seq = np.concatenate((dimer_seq, padding_vec))
+        # print(dimer_seq.shape)
+        # print(dimer_seq)
+        data.append([motif_pair_name, motif_pair_seq, dimer_name, dimer_seq, dimer_family])
+    return data
+
+def generate_decoder_input():
+
+
+
+def seq2seq_mt_model(encoder_input_data, decoder_input_data):
+    # define an input sequence and process it
+    batch_size = 1
+    epochs = 200
+    latent_dim = 128
+    input_dim = 4
+    target_dim = 6
+    encoder_inputs = Input(shape=(None, input_dim))
+    encoder = LSTM(latent_dim, return_state=True)
+    encoder_ouputs, state_h, state_c = encoder(encoder_inputs)
+
+    encoder_states = [state_h, state_c]
+
+    # Set up the decoder, using 'encoder_states' as initial state
+    decoder_inputs = Input(shape=(None, input_dim))
+
+    decoder_lstm = LSTM(latent_dim, return_squence=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = Dense(input_dim, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
+    #  Define the model that will turn
+    #  encoder_input_data & decoder_input_data into decoder_target_data
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    model.compile(optimizer=Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.001), loss='categorical_crossentropy')
+    model.fit([encoder_input_data, decoder_input_data], decoder_outputs,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split=0.2)
+
+
+
+
+
+
