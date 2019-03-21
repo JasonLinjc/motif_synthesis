8434aec93a2201e27aa032a8213aa6fa0d4ea4da
JasonLinjc <jieconlin3-c@my.cityu.edu.hk>
2019-03-21 22:22:53 +0800

    seq2seq attention start

--- seq2seq_atten.py
+++ seq2seq_atten.py
@@ -20,6 +20,25 @@ import random
 import tqdm
 import matplotlib.pyplot as plt
 
+def softmax(x, axis=1):
+    """
+    Softmax activation function.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
 
 def seq2seq_attention():
-    
+    Tx = 18
+    repeator = RepeatVector(Tx)
+    concatenator = Concatenate(axis=-1)
+    densor_tanh = Dense(32, activation="tanh")
+    densor_relu = Dense(1, activation="relu")
+    activator = Activation(softmax, name='attention_weights')
+    dotor = Dot(axes=1)
