4166df5cd543ede2bd1130f1319917649a012a25
JasonLinjc <jieconlin3-c@my.cityu.edu.hk>
2019-03-21 22:20:13 +0800

    nothing

--- seq2seq_atten.py
+++ seq2seq_atten.py
@@ -3,3 +3,23 @@
 # @Auther   :Jason Lin
 # @File     :seq2seq_atten.py
 # @Software :PyCharm
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from keras.layers import Bidirectional, Concatenate, Permute, Dot, Input, LSTM, Multiply, Reshape
+from keras.layers import RepeatVector, Dense, Activation, Lambda, Embedding
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from keras.models import load_model, Model
+import keras.backend as K
+import keras
+import numpy as np
+import pickle as pkl
+import random
+import tqdm
+import matplotlib.pyplot as plt
+
+
+def seq2seq_attention():
+    
