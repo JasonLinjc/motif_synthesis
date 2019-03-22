96d0ce804714b8b8388b10bf0018435c96db2b7c
JasonLinjc <jieconlin3-c@my.cityu.edu.hk>
2019-03-22 19:55:26 +0800

    w

--- motif_encoder_decoder.py
+++ motif_encoder_decoder.py
@@ -6,15 +6,75 @@
 
 import numpy as np
 import pandas as pd
+import pickle as pkl
 
 class motif_encoder:
     dimer_motif_data = pd.read_csv("./JiecongData/row_dimer_data.csv", index_col=[0])
+    single_motif_maxlen = 18
 
     def __init__(self, motif1_seq, motif2_seq, dimer_seq):
         self.motif1_seq = motif1_seq
         self.motif2_seq = motif2_seq
         self.dimer_seq = dimer_seq
+        self.encode_source_motif_pair()
 
 
+    def encode_source_motif_pair(self):
+        motif_pair_code = np.zeros((self.single_motif_maxlen*2, 4)) + 0.25
+        # motif_pair = np.concatenate((self.motif1_seq, self.motif2_seq), axis=0)
+        for i in range(len(self.motif1_seq)):
+            # code = np.zeros(self.max_pair_len * 4)
+            motif_pair_code[i,] = self.motif1_seq[i]
+            # pair_code.append(code)
+
+        for i in range(len(self.motif2_seq)):
+            motif_pair_code[i + self.single_motif_maxlen, ] = self.motif2_seq[i]
+        self.motif_pair_code = motif_pair_code
+
+    def encode_target_dimer(self):
+
+        pass
 
 
+def get_seq():
+
+    kc_dimer_info = pd.read_csv("./JiecongData/kc_dimer_info.csv")
+    homomotif_seq_dict = pkl.load(open("./JiecongData/homodimerMotifDatabase_dict.pkl", "rb"))
+    motif_seq_dict = pkl.load(open("JiecongData/motifDatabase_dict.pkl", "rb"))
+    dimer_seq_dict = pkl.load(open("JiecongData/dimerMotifDatabase_dict.pkl", "rb"))
+    dimerfamily_dict = pkl.load(open("JiecongData/dimerMotifFamily_dict.pkl", "rb"))
+
+    motif1_len = 0
+    dimer_len = 0
+    motif2_len = 0
+    for idx, d_info in kc_dimer_info.iterrows():
+        # if idx != 3:
+        #     continue
+        print(d_info)
+        olen = d_info['overlapLen']
+        isRC = d_info['isRC']
+        loc1 = d_info['loc1']
+        loc2 = d_info['loc2']
+        case = d_info['case']
+        motif1_name = d_info['name1']
+        motif2_name = d_info['name2']
+        dimer_name = d_info['nameOut']
+        dimer_seq = dimer_seq_dict[dimer_name]
+        try:
+            motif1_seq = motif_seq_dict[motif1_name]
+        except:
+            motif1_seq = homomotif_seq_dict[motif1_name]
+        try:
+            motif2_seq = motif_seq_dict[motif2_name]
+        except:
+            motif2_seq = homomotif_seq_dict[motif2_name]
+
+        # m = motif_encoder(motif1_seq, motif2_seq, dimer_seq)
+        # print(m.motif_pair_code)
+        # print(m.motif_pair_code.shape)
+        
+
+        break
+
+
+get_seq()
--- seq2seq_atten.py
+++ seq2seq_atten.py
@@ -20,6 +20,8 @@ import random
 import tqdm
 import matplotlib.pyplot as plt
 
+
+
 def softmax(x, axis=1):
     """
     Softmax activation function.
@@ -34,11 +36,99 @@ def softmax(x, axis=1):
     else:
         raise ValueError('Cannot apply softmax to a tensor that is 1D')
 
-def seq2seq_attention():
-    Tx = 18
-    repeator = RepeatVector(Tx)
-    concatenator = Concatenate(axis=-1)
-    densor_tanh = Dense(32, activation="tanh")
-    densor_relu = Dense(1, activation="relu")
-    activator = Activation(softmax, name='attention_weights')
-    dotor = Dot(axes=1)
+Tx = 18
+repeator = RepeatVector(Tx)
+concatenator = Concatenate(axis=-1)
+densor_tanh = Dense(32, activation = "tanh")
+densor_relu = Dense(1, activation = "relu")
+activator = Activation(softmax, name='attention_weights')
+dotor = Dot(axes = 1)
+
+def one_step_attention(a, s_prev):
+    """
+    Attention机制的实现，返回加权后的Context Vector
+
+    @param a: BiRNN的隐层状态
+    @param s_prev: Decoder端LSTM的上一轮隐层输出
+
+    Returns:
+    context: 加权后的Context Vector
+    """
+    # 将s_prev复制Tx次
+    s_prev = repeator(s_prev)
+    # 拼接BiRNN隐层状态与s_prev
+    concat = concatenator([a, s_prev])
+    # 计算energies
+    e = densor_tanh(concat)
+    energies = densor_relu(e)
+    # 计算weights
+    alphas = activator(energies)
+    # 加权得到Context Vector
+    context = dotor([alphas, a])
+
+    return context
+
+target_seq_dim = 6
+
+n_a = 32 # The hidden size of Bi-LSTM
+n_s = 128 # The hidden size of LSTM in Decoder
+decoder_LSTM_cell = LSTM(n_s, return_state=True)
+output_layer = Dense(target_seq_dim, activation=softmax)
+
+# 定义网络层对象（用在model函数中）
+reshapor = Reshape((1, target_seq_dim))
+concator = Concatenate(axis=-1)
+
+
+def model(Tx, Ty, n_a, n_s, source_seq_dim, target_seq_dim):
+    """
+    构造模型
+    @param Tx: 输入序列的长度
+    @param Ty: 输出序列的长度
+    @param n_a: Encoder端Bi-LSTM隐层结点数
+    @param n_s: Decoder端LSTM隐层结点数
+    @param source_vocab_size: 输入（英文）语料的词典大小
+    @param target_vocab_size: 输出（法语）语料的词典大小
+    """
+    # 定义输入层
+    X = Input(shape=(Tx,))
+    # Embedding层
+    # embed = embedding_layer(X)
+    # Decoder端LSTM的初始状态
+    s0 = Input(shape=(n_s,), name='s0')
+    c0 = Input(shape=(n_s,), name='c0')
+
+    # Decoder端LSTM的初始输入
+    out0 = Input(shape=(target_seq_dim,), name='out0')
+    out = reshapor(out0)
+
+    s = s0
+    c = c0
+
+    # 模型输出列表，用来存储翻译的结果
+    outputs = []
+
+    # 定义Bi-LSTM
+    a = Bidirectional(LSTM(n_a, return_sequences=True))(X)
+
+    # Decoder端，迭代Ty轮，每轮生成一个翻译结果
+    for t in range(Ty):
+        # 获取Context Vector
+        context = one_step_attention(a, s)
+
+        # 将Context Vector与上一轮的翻译结果进行concat
+        context = concator([context, reshapor(out)])
+        s, _, c = decoder_LSTM_cell(context, initial_state=[s, c])
+
+        # 将LSTM的输出结果与全连接层链接
+        out = output_layer(s)
+
+        # 存储输出结果
+        outputs.append(out)
+
+    model = Model([X, s0, c0, out0], outputs)
+
+    return model
+
+
+
